b83059351715a4f2d0b2ce279a3d1a41c955ee45
Sara Blackthorne <s.blackthorne@gmail.com>
2013-05-24 19:47:57 -0500

    Update il_corvo.rb

--- il_corvo.rb
+++ il_corvo.rb
@@ -1,5 +1,4 @@
-
 puts "Hello Greg."
-puts "It's a good thing you are cute because when you purposefully cause problems I have to go back and fix, sometimes I want to poke you, but instead I just smile because I think about how adorable you are and those deliciously kissable lips you have and all my anger melts away."
+puts "It's a good thing you are cute because when you purposefully cause problems I have to go back and fix, sometimes I want to poke you, but instead I just smile because I think about how adorable you are and all my anger melts away."
 puts "Hello World."
 
